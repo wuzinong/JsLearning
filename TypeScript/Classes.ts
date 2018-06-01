6fb4e672724e404b053c24056e2709de3a0685ca
wuzinong <wuzinong@gmail.com>
2018-06-01 17:35:39 +0800

    learning

---
+++ TypeScript/Classes.ts
@@ -0,0 +1,71 @@
+
+//Class
+class Greeter{
+    greeting:string;
+    constructor(message:string){
+        this.greeting = message;
+    }
+    greet(){
+        return "Hello, "+this.greeting;
+    }
+}
+let greeter = new Greeter("world"); 
+// 这个类有3个成员：一个叫做 greeting的属性，一个构造函数和一个 greet方法。
+// 你会注意到，我们在引用任何一个类成员的时候都用了 this。 它表示我们访问的是类的成员。
+// 最后一行，我们使用 new构造了 Greeter类的一个实例。 它会调用之前定义的构造函数，创建一个 Greeter类型的新对象，并执行构造函数初始化它。
+
+//继承
+//在TypeScript里，我们可以使用常用的面向对象模式。 基于类的程序设计中一种最基本的模式是允许使用继承来扩展现有的类
+
+class Animal{
+    move(distanceInMeters:number=0){
+        console.log(`Animal moved ${distanceInMeters} m`);
+    }
+}
+class Dog extends Animal{
+   bark(){
+       console.log("Woof!Woof!");
+   }
+}
+const dog = new Dog();
+dog.bark();
+dog.move(10);
+dog.bark();
+
+//这个例子展示了最基本的继承：类从基类中继承了属性和方法。 这里， Dog是一个 派生类，它派生自 Animal 基类，通过 extends关键字。 派生类通常被称作 子类，基类通常被称作 超类。
+//因为 Dog继承了 Animal的功能，因此我们可以创建一个 Dog的实例，它能够 bark()和 move()。
+//下面我们来看个更加复杂的例子。
+class Animal2{
+    name:string;
+    constructor(theName:string){this.name = theName;}
+    move(distanceInMeters:number=0){
+        console.log(`${this.name} moved ${distanceInMeters} m`);
+    }
+}
+class Snake extends Animal2{
+    constructor(name:string){super(name);}
+    move(distanceInMeters=5){
+        console.log("Slithering...");
+        super.move(distanceInMeters);
+    }
+}
+class Horse extends Animal2{
+    constructor(name:string){super(name);}
+    move(distanceInMeters=45){
+        console.log("Galloping...");
+        super.move(distanceInMeters);
+    }
+}
+let sam = new Snake("Sammy the Python");
+let tom:Animal2 = new Horse("Tommy the Palomino");
+sam.move();
+tom.move(34);
+
+//这个例子展示了一些上面没有提到的特性。 这一次，我们使用 extends关键字创建了 Animal的两个子类： Horse和 Snake。
+//与前一个例子的不同点是，派生类包含了一个构造函数，它 必须调用 super()，它会执行基类的构造函数。 而且，在构造函数里访问 this的属性之前，我们 一定要调用 super()。 这个是TypeScript强制执行的一条重要规则。
+//这个例子演示了如何在子类里可以重写父类的方法。 Snake类和 Horse类都创建了 move方法，它们重写了从 Animal继承来的 move方法，使得 move方法根据不同的类而具有不同的功能。 注意，即使 tom被声明为 Animal类型，但因为它的值是 Horse，调用 tom.move(34)时，它会调用 Horse里重写的方法：
+
+// Slithering...
+// Sammy the Python moved 5m.
+// Galloping...
+// Tommy the Palomino moved 34m.
--- TypeScript/interfaces.ts
+++ TypeScript/interfaces.ts
@@ -302,3 +302,6 @@ class Image implements SelectableControl {
 class Location {
 
 }
+//在上面的例子里，SelectableControl包含了Control的所有成员，包括私有成员state。 因为 state是私有成员，所以只能够是Control的子类们才能实现SelectableControl接口。 因为只有 Control的子类才能够拥有一个声明于Control的私有成员state，这对私有成员的兼容性是必需的。
+//在Control类内部，是允许通过SelectableControl的实例来访问私有成员state的。 实际上， SelectableControl接口和拥有select方法的Control类是一样的。 Button和TextBox类是SelectableControl的子类（因为它们都继承自Control并有select方法），但Image和Location类并不是这样的。
+
