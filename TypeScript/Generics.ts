913458e37ffb23955e5c6f81a386359466122474
wuzinong <wuzinong@gmail.com>
2018-06-06 17:23:05 +0800

    update

--- TypeScript/Generics.ts
+++ TypeScript/Generics.ts
@@ -73,6 +73,127 @@ function identity<T>(arg: T): T {
 }
 let myIdentity: {<T>(arg: T): T} = identity;
 
+//这引导我们去写第一个泛型接口了。 我们把上面例子里的对象字面量拿出来做为一个接口：
+interface GenericIdentityFn{
+    <T>(arg:T):T;
+}
+function identity<T>(arg:T):T{
+    return arg;
+}
+let myIdentity:GenericIdentityFn = identity;
+//一个相似的例子，我们可能想把泛型参数当作整个接口的一个参数。 这样我们就能清楚的知道使用的具体是哪个泛型类型（比如： Dictionary<string>而不只是Dictionary）。 这样接口里的其它成员也能知道这个参数的类型了。
+
+interface GenericIdentityFn2<T>{
+    (arg:T):T;
+}
+function identity2<T>(arg:T):T{
+    return arg;
+}
+let myIdentity2:GenericIdentityFn2<number> = identity2;
+
+//注意，我们的示例做了少许改动。 不再描述泛型函数，而是把非泛型函数签名作为泛型类型一部分。 当我们使用 GenericIdentityFn的时候，还得传入一个类型参数来指定泛型类型（这里是：number），锁定了之后代码里使用的类型。 对于描述哪部分类型属于泛型部分来说，理解何时把参数放在调用签名里和何时放在接口上是很有帮助的。
+//除了泛型接口，我们还可以创建泛型类。 注意，无法创建泛型枚举和泛型命名空间。
+
+//泛型类
+//泛型类看上去与泛型接口差不多。 泛型类使用（ <>）括起泛型类型，跟在类名后面。
+class GenericNumber<T>{
+    zeroValue:T;
+    add: (x: T, y: T) => T;
+}
+let myGenericNumber = new GenericNumber<number>();
+myGenericNumber.zeroValue = 0;
+myGenericNumber.add = function(x, y) { return x + y; };
+
+//GenericNumber类的使用是十分直观的，并且你可能已经注意到了，没有什么去限制它只能使用number类型。 也可以使用字符串或其它更复杂的类型。
+let stringNumeric = new GenericNumber<string>();
+stringNumeric.zeroValue = "";
+stringNumeric.add = function(x, y) { return x + y; };
+
+alert(stringNumeric.add(stringNumeric.zeroValue, "test"));
+
+//与接口一样，直接把泛型类型放在类后面，可以帮助我们确认类的所有属性都在使用相同的类型。
+//我们在类那节说过，类有两部分：静态部分和实例部分。 泛型类指的是实例部分的类型，所以类的静态属性不能使用这个泛型类型
+
+//泛型约束
+//你应该会记得之前的一个例子，我们有时候想操作某类型的一组值，并且我们知道这组值具有什么样的属性。 在 loggingIdentity例子中，我们想访问arg的length属性，但是编译器并不能证明每种类型都有length属性，所以就报错了。
+function loggingIdentity<T>(arg: T): T {
+    console.log(arg.length);  // Error: T doesn't have .length
+    return arg;
+}
+
+//相比于操作any所有类型，我们想要限制函数去处理任意带有.length属性的所有类型。 只要传入的类型有这个属性，我们就允许，就是说至少包含这一属性。 为此，我们需要列出对于T的约束要求。
+// 为此，我们定义一个接口来描述约束条件。 创建一个包含 .length属性的接口，使用这个接口和extends关键字来实现约束：
+interface Lengthwise {
+    length: number;
+}
+
+function loggingIdentity<T extends Lengthwise>(arg: T): T {
+    console.log(arg.length);  // Now we know it has a .length property, so no more error
+    return arg;
+}
+//现在这个泛型函数被定义了约束，因此它不再是适用于任意类型：
+loggingIdentity(3);  // Error, number doesn't have a .length property
+
+//我们需要传入符合约束类型的值，必须包含必须的属性：
+loggingIdentity({length: 10, value: 3});
+
+//在泛型约束中使用类型参数
+//你可以声明一个类型参数，且它被另一个类型参数所约束。 比如，现在我们想要用属性名从对象里获取这个属性。 并且我们想要确保这个属性存在于对象 obj上，因此我们需要在这两个类型之间使用约束。
+
+function getProperty(obj: T, key: K) {
+    return obj[key];
+}
+
+let x = { a: 1, b: 2, c: 3, d: 4 };
+
+getProperty(x, "a"); // okay
+getProperty(x, "m"); // error: Argument of type 'm' isn't assignable to 'a' | 'b' | 'c' | 'd'.
+
+//在泛型里使用类类型
+//在TypeScript使用泛型创建工厂函数时，需要引用构造函数的类类型。比如，
+
+function create<T>(c: {new(): T; }): T {
+    return new c();
+}
+
+//一个更高级的例子，使用原型属性推断并约束构造函数与类实例的关系。
+class BeeKeeper {
+    hasMask: boolean;
+}
+
+class ZooKeeper {
+    nametag: string;
+}
+
+class Animal {
+    numLegs: number;
+}
+
+class Bee extends Animal {
+    keeper: BeeKeeper;
+}
+
+class Lion extends Animal {
+    keeper: ZooKeeper;
+}
+
+function createInstance<A extends Animal>(c: new () => A): A {
+    return new c();
+}
+
+createInstance(Lion).keeper.nametag;  // typechecks!
+createInstance(Bee).keeper.hasMask;   // typechecks!
+
+
+
+
+
+
+
+
+
+
+
 
 
 
