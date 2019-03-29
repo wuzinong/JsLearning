0e1b05f1def581259d26fa3e7f80b1a44fd305ee
wuzinong <wuzinong@gmail.com>
2019-03-29 17:11:12 +0800

    update

--- DotNetCoreSingleApplication/ServerSideRendering/Scripts/HelloWorld.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export class HelloWorld extends React.Component{
-    render(){
-        return <div>
-            Hello world using React
-        </div>
-    }
-}
--- DotNetCoreSingleApplication/ServerSideRendering/Scripts/entry.jsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-import {render} from 'react-dom';
-import {HelloWorld} from './HelloWorld';
-
-render(<HelloWorld/>,document.querySelector("body"));
