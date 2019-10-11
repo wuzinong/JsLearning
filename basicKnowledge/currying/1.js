c2fd8ef6603b35fe081d1ff05432036cb2f96ea9
wuzinong <wuzinong@gmail.com>
2019-10-11 17:23:14 +0800

    udpate

--- basicKnowledge/currying/1.js
+++ basicKnowledge/currying/1.js
@@ -123,3 +123,45 @@ console.log(a(10) + 100);  // 120
 console.log(b(10) + 100);  // 120
 console.log(c(10) + 100);  // 120
 console.log(d(10) + 100);  // 120
+
+
+// 公式类型二: 无参数传入时并且参数数量已经满足函数要求
+// fn(a, b, c, d) => fn(a)(b)(c)(d)();
+// fn(a, b, c, d) => fn(a);fn(b);fn(c);fn(d);fn();
+
+const createCurry = (fn, ...args) => {
+    let all = args || [];
+    let length = fn.length;
+
+    return (...rest) => {
+        let _allArgs = all.slice(0);
+        _allArgs.push(...rest);
+        if (rest.length > 0 || _allArgs.length < length) {
+            // 调用时参数不为空或存储的参数不满足原始函数参数数量时，返回curry函数
+            return createCurry.call(this, fn, ..._allArgs);
+        } else {
+            // 调用参数为空(),且参数数量满足时，触发执行
+            return fn.apply(this, _allArgs);
+        }
+    }
+}
+const curryAdd = createCurry(add, 2);
+let sum = curryAdd(3)(4)(5)();  // 14
+
+// ES5写法
+function createCurry() {
+    var fn = arguments[0];
+    var _args = [].slice.call(arguments, 1);
+    var length = fn.length;
+    
+    return function() {
+        var _allArgs = _args.slice(0);
+        _allArgs = _allArgs.concat([].slice.call(arguments));
+        if (arguments.length > 0 || _allArgs.length < length) {
+            _allArgs.unshift(fn);
+            return createCurry.apply(this, _allArgs);
+        } else {
+            return fn.apply(this, _allArgs);
+        }
+    }
+}
