b5a46c962e943ec4cb25517a6c7b56560b877479
Gu <Xie.Hai.Bran.Gu@dnv.com>
2023-01-31 10:58:14 +0800

    update

--- TypeScript/challenge/exclude copy.ts
+++ TypeScript/challenge/exclude copy.ts
@@ -1,5 +1,5 @@
 type MyExclude2<T, U> = any;
 
 type A1 = "a" | "b" | "c";
-type B1 = "a";
-type C1 = MyExclude2<A1, B1>; // C = "b" | "c"
+type B1 = "a" | "b";
+type C1 = MyExclude2<A1, B1>; // C1 = "b" | "c"
